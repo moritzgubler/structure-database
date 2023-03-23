ea36dacfcc1237e1730ce0ae1ca641391b20d842
moritzgubler <moritz.gubler@gmail.com>
2023-03-23 12:03:43 +0100

    subparser working

--- structureDB/main.py
+++ structureDB/main.py
@@ -9,9 +9,15 @@ def main():
         """ 
     )
     subparsers = parser.add_subparsers(help="Command that database will execute")
-    add_parser = subparsers.add_parser("add")
-    conv_parser = subparsers.add_parser("convexHull")
-    get_parser = subparsers.add_parser("get")
+    add_parser = subparsers.add_parser("add", help="Add a list of files to the database")
+    conv_parser = subparsers.add_parser("convexHull", help = "Calculate a convex hull plot")
+    init_parser = subparsers.add_parser("init", help = "Initialize a structure database in the current working derectory")
+    # get_parser = subparsers.add_parser("get", help="Deprecated")
+
+    add_parser.add_argument('files', type=argparse.FileType('r'), nargs=argparse.REMAINDER)
+
+
+    # add_args = add_parser.parse_args()
     args = parser.parse_args()
 
-    print(args.command)
+    print(args.files)
