562ba032d704b33af0b65b3039275121bcadfe1b
moritzgubler <moritz.gubler@gmail.com>
2023-03-23 10:28:54 +0100

    added sub parser

--- structureDB/main.py
+++ structureDB/main.py
@@ -2,4 +2,16 @@ import argparse
 from ase.io import read, write
 
 def main():
-    print('hello world')
+    
+    parser = argparse.ArgumentParser(description = 
+        """
+        Welcome to structureDB. This program organizes structures using the ASE IO functions and organizes them.
+        """ 
+    )
+    subparsers = parser.add_subparsers(help="Command that database will execute")
+    add_parser = subparsers.add_parser("add")
+    conv_parser = subparsers.add_parser("convexHull")
+    get_parser = subparsers.add_parser("get")
+    args = parser.parse_args()
+
+    print(args.command)
