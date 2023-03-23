6ab3250cd7e9dfb5ec45ee12652b248f5c9d3059
moritzgubler <moritz.gubler@gmail.com>
2023-03-23 14:42:03 +0100

    finished read_data, started add data

--- structureDB/database.py
+++ structureDB/database.py
@@ -8,9 +8,18 @@ database_path = "structureDB/"
 
 def read_structure_database():
     files = find_files()
+    database = {}
+    for f in files:
+        stoichio = f.split('.')[0]
+        database[stoichio] = read(f, index=':')
+    return database
 
 def find_files():
     return os.listdir(structureDB.database.database_path)
 
 def getStoichiometry(struct: Atoms):
     return ase.formula.Formula.from_list(struct.get_chemical_symbols()).format('hill')
+
+def addAtoms(atomList: list):
+    for atom in atomList:
+        ...
