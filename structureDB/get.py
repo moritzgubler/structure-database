3b28e6ce5d3f7a9c740e20e4cce2624c83f6bb05
moritzgubler <moritz.gubler@gmail.com>
2023-03-24 08:22:13 +0100

    fix energy range and delete emtpy output driectory

--- structureDB/get.py
+++ structureDB/get.py
@@ -1,6 +1,7 @@
 import structureDB.database
 from ase.io import write
 import os
+import shutil
 
 allowed_extensions = ['ascii', 'extxyz', 'in']
 
@@ -11,12 +12,19 @@ concatanable = {
 }
 
 def get(emax, nmax, outdir, extension):
+    # delete output file or directory if present.
+    if os.path.isfile(outdir) or os.path.islink(outdir):
+        os.remove(outdir)
+    elif os.path.isdir(outdir):
+        shutil.rmtree(outdir)
+    
+    os.mkdir(outdir)
     db = structureDB.database.read_structure_database()
     for st in db:
         i = 0
         at_list = []
         while i < len(db[st]):
-            if i >= nmax or db[st][i].info['energy'] - db[st][0].info['energy'] > emax:
+            if i >= nmax or (db[st][i].info['energy'] - db[st][0].info['energy']) / len(db[st][0]) > emax:
                 break
             at_list.append(db[st][i])
             i+=1
